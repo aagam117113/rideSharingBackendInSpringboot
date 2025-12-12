cc7552da8ab2eb4b04d7f28c5dafd653b0f7d8e2
aagam117113 <107100247+aagam117113@users.noreply.github.com>
2025-12-12 16:17:03 +0530

    final

---
+++ tripmate/exception/NotFoundEx.java
@@ -0,0 +1,9 @@
+package com.student.tripmate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundEx extends RuntimeException {
+    public NotFoundEx(String m) { super(m); }
+}
